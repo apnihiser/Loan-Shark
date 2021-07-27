6462ff9412588fe7bbab2a79bc12771b09b2668e
merkkie <apnihiser@gmail.com>
2021-07-27 19:43:26 -0400

    completed logic on javascript

--- js/site.js
+++ js/site.js
@@ -1,41 +1,3 @@
-// Monthly Payment = 
-// Monthly Mortgatge payment (m) = (principle (p) * interest rate(r)( 1 + r) ^ total number of payments(n)) / (( 1 + interest rate r) ^ total number of payments (n) - 1)
-// or
-// m = (P * r( 1 + r) ^ n) / (( 1 + r) ^ n - 1 )
-
-// p = 300,000
-// r = 5% or .05 annualy interest rate
-// r = 0.05 / 12 to get monthly interest rate
-// n = 30 * 12 = 360
-// M = 300,000 (.05/12)(1+.05/12) ^ 360 / (1+.05/12) ^ 360 - 1
-
-// M = 300,000 (.05/12)(1+.05/12) ^ 360 / (1+.05/12) ^ 360 - 1
-// m = 1250 * 4.467744314 / 3.467744314
-// m = 1,610.46
-
-
-//can also use m = p * r / 1 - (1 + r) ^ -n
-// m = 300,000 * 0.05/12  / 1 - (1 + 0.05/12) ^ -360
-// m = 1250 / .7761734044
-// m = $1610.46
-
-// T = total amount of money that she will have to repay.
-// T = $1610.46 * 360 = $579,765.60
-
-// Total interest I = Total amout of money - P
-// $579,765.60 - 300,000 = $279,765.60
-
-// Coder F Math
-//
-// rate = 5% or .05 yearly rate or .05/12 for monthly rate 
-// loanAmount = 25000
-// term = 60
-//
-// m = (25000) * (.05/12) / (1 - (1 + .05/12) -60)
-// m = 25000 * 0.004166666 / (1 - (1 + 0.004166666) ^ -60)
-// m = 104.166666666 / 0.220794609
-// m = $471.78
-
 //Controller function
 //gather data from webform
 function getValues() {
@@ -48,7 +10,6 @@ function getValues() {
     //define loan rate / 100 per month / per days
     loanTerms.rate = parseInt(document.getElementById("inputInterestRate").value);
     
-
     //form is validating numbers but we do a check here to ensure there is a value provided as well
     if ( !Number.isInteger(loanTerms.principal) || !Number.isInteger(loanTerms.numberMonths) || !Number.isInteger(loanTerms.rate) ) {
 
@@ -95,6 +56,17 @@ function generateTable(loanRequest) {
 
 function displayValues(loanTerms, amorTable) {
 
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+      })
+
+    document.getElementById("payment").innerHTML =  formatter.format(amorTable.monthlyPayment);
+    document.getElementById("principal").innerHTML = formatter.format(loanTerms.principal);
+    document.getElementById("interest").innerHTML = formatter.format(amorTable.totalInterest);
+    document.getElementById("cost").innerHTML = formatter.format(amorTable.totalCost);
+
     //get the table body element from the page
     let tableBody = document.getElementById("results");
 
@@ -120,11 +92,11 @@ function displayValues(loanTerms, amorTable) {
         
 
         rowCols[0].textContent = i;
-        rowCols[1].textContent = Math.round( amorTable.monthlyPayment * 100) / 100 ;
-        rowCols[2].textContent = Math.ceil( principalPayment * 100) / 100;
-        rowCols[3].textContent = Math.ceil( interestPayment * 100) / 100;
-        rowCols[4].textContent = Math.ceil( amorTable.currentInterest * 100) / 100;
-        rowCols[5].textContent = Math.ceil( amorTable.balance * 100) / 100;
+        rowCols[1].textContent = formatter.format(amorTable.monthlyPayment);
+        rowCols[2].textContent = formatter.format(principalPayment);
+        rowCols[3].textContent = formatter.format(interestPayment);
+        rowCols[4].textContent = formatter.format(amorTable.currentInterest);
+        rowCols[5].textContent = formatter.format(amorTable.balance);
 
         tableBody.appendChild(tableRow);
     }
